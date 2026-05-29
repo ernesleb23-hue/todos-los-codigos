98255b75b957854bbbbb947a89ebcdfcb95b5a01
ernesleb23-hue <ernesleb23@gmail.com>
2026-05-28 18:37:02 -0600

    Update codigo4.cpp

--- codigo4.cpp
+++ codigo4.cpp
@@ -7,23 +7,15 @@ int main() {
 	string nombre;
 	string colonia;
 	long cp;
-	string pais;
-	string planeta;
 	cout<<"introduzca el nombre de la persona"<<endl;
 	cin>>nombre;
 	cout<<"introduzca la colonia de la persona"<<endl;
 	cin>>colonia;
 	cout<<"introduzca el codigo postal de la persona"<<endl;
 	cin>>cp;
-	cout<<"introduzca el pais donde vive la persona"<<endl;
-	cin>>pais;
-	cout<<"introduzca el planeta donde vive la persona"<<endl;
-	cin>>planeta;
 	cout<< "el nombre de la persona es: "<<nombre<<endl;
 	cout<< "la colonia de la persona es: "<<colonia<<endl;
 	cout<< "el codigo postal de la persona es: "<<cp<<endl;
-	cout<< "el pais donde vive la persona es: "<<pais<<endl;
-	cout<< "el planeta donde vive la persona es: "<<planeta<<endl;
 	system("pause");
 	
 	return 0;
