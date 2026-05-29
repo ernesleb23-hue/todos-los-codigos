c02137d7115cc980e64c3508d4a19bd4368101a5
ernesleb23-hue <ernesleb23@gmail.com>
2026-05-28 18:34:51 -0600

    Update codigo16.cpp

--- codigo16.cpp
+++ codigo16.cpp
@@ -4,21 +4,21 @@ using namespace std;
 
 int main() {
 	int opcion;
-	cout<<"programacion\n 1)java\n 2)python\n 3)c++\n elige un lenguaje: ";
+	cout<<"office\n 1)word\n 2)powerpoint 3)excel\n elige un programa: ";
 	cin>>opcion;
 	switch (opcion)
 	{
 	case 1:
-		cout<<"\n\n seleccionaste java\n";
+		cout<<"\n\n seleccionaste word\n";
 		break;
 	case 2:
-		cout<<"\n\n seleccionaste python\n";
+		cout<<"\n\n seleccionaste powerpoint\n";
 		break;
 	case 3:
-		cout<<"\n\n seleccionaste c++\n";
+		cout<<"\n\n seleccionaste excel\n";
 		break;
 	default:
-		cout<<"\n\n\n lenguaje no valido\n";
+		cout<<"\n\n\n programa no valido\n";
 		Beep(750,3500);
 		std::cout<<"\n\n\n adios";
 	}
